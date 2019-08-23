ecb6e2bafb4392df8942771cc2c57687718d938b
k5i ogura <kenji.ogura@jcom.zaq.ne.jp>
2019-08-23 19:00:19 +0900

    "revise"

--- application001/sample2.py
+++ application001/sample2.py
@@ -1,7 +1,7 @@
 import chainer
 import numpy as np
 import chainer.computational_graph as c
-import csv
+import sys,os,argparse
 from chainer import serializers
 from userhook import UserHook
 import chainer.functions as F
@@ -24,7 +24,7 @@ def infer(inp):
 #    inp=0
     a = txs[:inp]
     print("inp=",inp)
-    print("gt:input number = " ,tts[:inp])
+    print("gt:input number  =" ,tts[:inp])
 
     x = txs[:inp].reshape((-1,28,28,1))
 
@@ -36,9 +36,12 @@ def infer(inp):
 
     ans = np.argmax(softmax_a.data, axis=1)
     #idx = int(ans.data)
-    print("pr:answer =",ans)
+    print("pr:answer number =",ans)
 
 if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument('-i','--images',type=int,default=len(tts))
+    args = args.parse_args()
     var.n = -1 # No fault injection for Normal System case
-    infer(inp=len(tts))
+    infer(inp=args.images)
     copy_tree('dnn_params', 'original_data2')
