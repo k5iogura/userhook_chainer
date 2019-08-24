abaffbb07cf46ffa9b227d6ce475107d6db1b96e
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-08-24 07:29:56 +0000

    revise

---
+++ application002/call2.py
@@ -0,0 +1,47 @@
+from distutils.dir_util import copy_tree
+from shutil import copy2
+
+#import subprocess
+#from replace_sample import replace_sample
+# from replace_userfunc import replace_userfunc
+
+import sample2                          # append
+tsx, tts = sample2.txs, sample2.tts     # append
+data_N   = len(tts)                     # append
+from userfunc_var import VAR            # append
+var = VAR()                             # append
+
+#from distutils.dir_util import copy_tree
+#import shutil
+from mylist_block import mylist
+
+#パラメータの変更(壊す場所の変更)＠userfunc.py
+
+d=mylist()
+#print(d)
+for k in range(784):              # k:fault pattern No.
+
+    var.n = k
+#    if(k!=0):
+#        replace_userfunc(k)
+
+    print("mylist=",d[k])   
+#    for i in range(0,2):       # i:number of feeding image
+    data_P = 10000              # data_P:amount of images a inference
+    data_P = 20
+        
+#        if(i!=0):
+#            replace_sample(i)
+#    print("input picture no=" ,i)
+#        subprocess.run("python3 sample.py",shell =True)
+    sample2.infer(data_P)
+    print('sample計算完了')
+#        rename =  "list" + str(k)+"_no-"+str(i)
+    rename =  "list%d_no%d-%d"%(k, 0, data_P-1)
+#    rename2 = "cp -fr dnn_params "+ rename
+#    subprocess.run(rename2,shell =True)
+    copy_tree("dnn_params", rename)
+#    subprocess.run("cp sample_origin.py sample.py",shell =True)
+        
+#subprocess.run("cp userfunc_origin.py userfunc.py",shell =True)
+#subprocess.run("python3 heatmap.py",shell =True)
---
+++ application002/heatmap2.py
@@ -0,0 +1,98 @@
+import os,sys
+from   pdb import *
+import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import chainer
+from chainer import Variable
+
+# loadin from .npy
+def load_npz(gdir):
+    #print("loading",gdir)
+    assert os.path.exists(gdir), "Not found `{}` directory".format(gdir)
+    cwd = os.getcwd()
+    os.chdir(gdir)
+    a = np.load('lx1_Linear_out.npy') 
+    a = np.reshape(a, (-1, 50, 1))
+    b = np.load('ly2_Linear_in.npy')
+    b = np.reshape(b, (-1, 50, 1))
+    c = np.load('ly2_Linear_out.npy')
+    c = np.reshape(c, (-1, 50, 1))
+    d = np.load('lz3_Linear_in.npy')
+    d = np.reshape(d, (-1, 50, 1))
+    os.chdir(cwd)
+    return np.concatenate((a, b, c, d), axis=2)
+
+# loading normal system result
+def load_normal(gdir):
+    x= load_npz(gdir)
+    return x
+
+# loading faults system result
+def load_faults(faultNo_list, Nimage):
+    hm  = np.zeros((max(faultNo_list)+1, Nimage, 50, 4 )) # hm.shape = (faultNo, imageNo, 50, 4 )
+    for faultNo in faultNo_list:
+        fault_dir = "list%d_no0-%d"%(faultNo, Nimage-1)
+        h = load_npz(fault_dir)
+        hm[faultNo] = h
+    return hm
+
+# spec to load
+faultNo_list = list(set([10,0,1]))              # ex. Fault-wise
+faultNo_list = range(784)                       # ex. About all faults
+data_P = 20
+normal  = 'original_data2'
+
+# load normal and fault systems
+nmap = load_normal(normal)                      # nmap.shape = (imageNo, 50, 4)
+fmap = load_faults(faultNo_list, data_P)        # fmap.shape = (faultNo, imageNo, 50, 4)
+
+#jは推論する画像の番号、iは壊す範囲の左上座標のリストでの番号
+
+hm_sum= np.zeros((50,4),dtype=np.float64)
+
+for j in range(0,data_P): #オリジナルから行列を作成  # j:number of image
+    # Normal System
+    hm_a = nmap[j]
+
+    for i in faultNo_list:
+        hm_b = fmap[i][j]
+        assert hm_b.shape == hm_a.shape,"Internal Error"
+
+        hm = hm_a-hm_b
+        zettaichi = np.abs(hm)
+        hm_sum = zettaichi + hm_sum
+print(np.sum(nmap[:data_P]),np.sum(fmap[:784][:data_P]))
+print(np.sum(hm_sum))
+sys.exit(-1)
+
+#型変換等
+mylist=[0]
+del mylist[0]
+for k in range(50):
+    for l in range(4):
+        a='variable' in str(hm_sum[k,l])
+        if a==True:
+            s = str(hm_sum[k,l]).split('(')
+            s = s[1].split(')')
+            s = s[0]
+            #print(s)
+            mylist.append(s)
+        else:
+            mylist.append(str(hm_sum[k,l]))
+
+mylist_f = [float(s) for s in mylist]
+mylist = np.array(mylist_f)
+mylist = np.reshape(mylist, (50, 4))
+
+
+#ヒートマップをつくる、表示する
+heatmap = sns.heatmap(mylist)
+plt.show(heatmap)
+
+
+    
+
+    
