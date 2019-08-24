5dc9b8f9728e2ebde2608b5d72303b41ea04cf7d
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-08-24 05:30:04 +0000

    revise

---
+++ application001/mylist_block.py
@@ -0,0 +1,12 @@
+def mylist():
+    mlist=[]
+    for i in range(7):
+        for j in range(7):
+            for k in range(0,28,7):
+                for l in range(0,28,7):
+                    mlist.append([i+k,j+l])
+    for i in range(7*7*4*4):
+        print(mlist[i])
+    return mlist
+if __name__=='__main__':
+    mylist()
