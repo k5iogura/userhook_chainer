a2231792cc8c256492eb8689172f0dcf1dcb8818
Kenji Ogura <k5i.ogura.v40@gmail.com>
2019-12-12 16:18:39 +0900

    revise

--- atpg001/call2.py
+++ atpg001/call2.py
@@ -2,10 +2,6 @@ from distutils.dir_util import copy_tree
 from shutil import rmtree
 import os,sys,argparse
 
-#import subprocess
-#from replace_sample import replace_sample
-# from replace_userfunc import replace_userfunc
-
 import sample2
 from userfunc_var import VAR
 var = VAR()
@@ -23,30 +19,13 @@ print("Run on Faults as",faultNo_list)
 from mylist_block import mylist
 
 d=mylist()
-#print(d)
-#for k in range(784):              # k:fault pattern No.
 for k in faultNo_list:
 
     var.n = k
-#    if(k!=0):
-#        replace_userfunc(k)
 
     print("mylist=",d[k],k)   
-#    for i in range(0,2):       # i:number of feeding image
-        
-#        if(i!=0):
-#            replace_sample(i)
-#    print("input picture no=" ,i)
-#        subprocess.run("python3 sample.py",shell =True)
     sample2.infer(data_P)
     print('sample done')
-#        rename =  "list" + str(k)+"_no-"+str(i)
     rename =  "list%d"%(k)
-#    rename2 = "cp -fr dnn_params "+ rename
-#    subprocess.run(rename2,shell =True)
     rmtree(rename, ignore_errors=True)
     copy_tree("dnn_params", rename)
-#    subprocess.run("cp sample_origin.py sample.py",shell =True)
-        
-#subprocess.run("cp userfunc_origin.py userfunc.py",shell =True)
-#subprocess.run("python3 heatmap.py",shell =True)
